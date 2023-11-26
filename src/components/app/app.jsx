f18ee864a172e797a853d5181a755ba1c5a26100
WanomiR <wanomir@yandex.ru>
2023-11-26 12:55:25 +0300

    header markup

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,12 @@
 import styles from "./app.module.css";
 import {data} from "../../utils/data";
 
+import AppHeader from "../header/header";
+
 function App() {
     return (
         <div className={styles.app}>
-      <pre style={{
-          margin: "auto",
-          fontSize: "1.5rem"
-      }}>
-      	Измените src/components/app/app.jsx и сохраните для обновления.
-          <br/>New line of text.
-      </pre>
+            <AppHeader/>
         </div>
     );
 }
---
+++ src/components/header/header.jsx
@@ -0,0 +1,37 @@
+import styles from "./header.module.css"
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { useState} from "react";
+
+const NavItem = ({ Icon, children, ...props}) => {
+    const [isActive, setIsActive] = useState(false)
+
+    const clickHandler = () => setIsActive(!isActive)
+
+    const type = isActive ? "primary" : "secondary"
+    const textInactive = isActive ? null : "text_color_inactive"
+
+    return (
+        <li className={`${props.className} p-5`}>
+            <a className={styles.link} href={"#"} onClick={clickHandler}>
+                <Icon type={type}/><span className={`ml-2 text_type_main-default ${textInactive}`}>{children}</span>
+            </a>
+        </li>
+    )
+}
+
+export  default function AppHeader() {
+    return (
+        <header className={`${styles.header} p-4`}>
+            <nav className={styles.navigation}>
+                <ul className={styles.list}>
+                    <div className={styles.container}>
+                        <NavItem Icon={BurgerIcon}>Конструктор</NavItem>
+                        <NavItem Icon={ListIcon} className={"ml-2"}>Лента заказов</NavItem>
+                    </div>
+                    <NavItem Icon={ProfileIcon}>Личный кабинет</NavItem>
+                </ul>
+                <a className={styles.logo}><Logo /></a>
+            </nav>
+        </header>
+    )
+}
